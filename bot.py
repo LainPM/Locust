30f6bbb7a2b23079961e9c099f9e3a399406346a
LainPM <159606365+LainPM@users.noreply.github.com>
2025-04-27 15:03:43 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,15 @@ import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+# 1. Configure intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# 2. Create the bot with intents
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
@@ -12,4 +20,5 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
+# 3. Run using the token from your environment
 bot.run(os.getenv("DISCORD_TOKEN"))
