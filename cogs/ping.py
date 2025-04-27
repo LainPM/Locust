ba1350fc526fb95f163343fe5171cd3575f6f222
LainPM <159606365+LainPM@users.noreply.github.com>
2025-04-27 15:38:29 -0700

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,14 +1,15 @@
+# cogs/ping.py
 import discord
 from discord.ext import commands
 from discord import app_commands
 
-class Example(commands.Cog):
+class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="ping", description="Ping the bot.")
+    @app_commands.command(name="ping", description="Replies with Pong!")
     async def ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message("🏓 Pong! ERWERRERER")
+        await interaction.response.send_message("🏓 Pong!")
 
 async def setup(bot):
-    await bot.add_cog(Example(bot))
+    await bot.add_cog(Ping(bot))
