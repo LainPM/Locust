edd961b62469f2e9935630fd1e9a71a581ecb462
LainPM <159606365+LainPM@users.noreply.github.com>
2025-05-03 02:35:13 -0700

    Update __init__.py

--- systems/starboard/__init__.py
+++ systems/starboard/__init__.py
@@ -38,4 +38,33 @@ class StarboardSystem(System):
     
     async def load_settings(self):
         """Load settings from database"""
-        collection = s
+        collection = self.bot.db["starboard_settings"]
+        async for doc in collection.find({}):
+            guild_id = doc["guild_id"]
+            self.settings_cache[guild_id] = doc
+        
+        print(f"Loaded starboard settings for {len(self.settings_cache)} guilds")
+    
+    async def get_settings(self, guild_id: int):
+        """Get settings for a guild"""
+        if guild_id not in self.settings_cache:
+            # Load or create default settings
+            settings = await self.storage.get_settings(guild_id)
+            self.settings_cache[guild_id] = settings
+        
+        return self.settings_cache[guild_id]
+    
+    async def start_reaction_tasks(self):
+        """Start background tasks for checking reactions"""
+        for guild_id in self.settings_cache:
+            if guild_id not in self.reaction_tasks or self.reaction_tasks[guild_id].done():
+                self.reaction_tasks[guild_id] = self.bot.loop.create_task(
+                    self.processor.check_reactions_task(guild_id)
+                )
+    
+    async def cleanup(self):
+        """Clean up resources when bot is shutting down"""
+        # Cancel all running tasks
+        for task in self.reaction_tasks.values():
+            if not task.done():
+                task.cancel()
